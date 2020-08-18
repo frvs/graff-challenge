9bd4a8e6a16e540117ebc737706d8c7d62ae7caa
Lucas Frois <ffrois.lucas@gmail.com>
2020-08-18 15:24:35 -0300

    Criando repositório genérico

---
+++ Graff.Api/Repositories/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Graff.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Graff.Api.Repositories.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Bid> Bid { get; set; }
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Graff.Api/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Graff.Api.Entities.ValueObjects;
+using Graff.Api.Repositories.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Graff.Api.Repositories
+{
+    public class GenericRepository<T> where T: Entity
+    {
+        private DataContext _context;
+
+        public GenericRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public string Create(T obj)
+        {
+            _context.Set<T>().Add(obj);
+            var rowsAffected = _context.SaveChanges();
+
+            return rowsAffected == 1 ? obj.Id : string.Empty;
+        }
+       
+        public void Update(T obj)
+        {
+            _context.Set<T>().Update(obj);
+            _context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var entity = GetById(id);
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public T GetById(string id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().AsNoTracking().ToList();
+        }
+    }
+}
