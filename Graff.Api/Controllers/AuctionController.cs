b65f0467d4f0c7ba07d891a265d0d97e1fca6883
Lucas Frois <ffrois.lucas@gmail.com>
2020-08-18 15:24:09 -0300

    Criando controllers

---
+++ Graff.Api/Controllers/AuctionController.cs
@@ -0,0 +1,40 @@
+﻿using Graff.Api.Controllers.Dtos;
+using Graff.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Graff.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        private AuctionService _auctionService;
+
+        public IActionResult AddBid([FromBody] NewBidDto dto)
+        {
+            try
+            {
+                var response = _auctionService.AddNewBid(dto.PersonId, dto.ProductId, dto.Value);
+                return Created(response.Id, response);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        public IActionResult GetBidsByPerson([FromQuery] string personId)
+        {
+            try
+            {
+                var response = _auctionService.GetBidsByPersonId(personId);
+                return Ok(response);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+    }
+}
---
+++ Graff.Api/Controllers/Dtos/NewBidDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Graff.Api.Controllers.Dtos
+{
+    public class NewBidDto
+    {
+        public string PersonId { get; set; }
+        public string ProductId { get; set; }
+        public decimal Value { get; set; }
+    }
+}
