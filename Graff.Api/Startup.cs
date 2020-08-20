533a88aad9e4c4ef743e760050e5abcee22c0a71
Lucas Frois <ffrois.lucas@gmail.com>
2020-08-19 23:51:09 -0300

    Adicionando CORS.

--- Graff.Api/Startup.cs
+++ Graff.Api/Startup.cs
@@ -24,7 +24,15 @@ namespace Graff.Api
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {            
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin();
+                    });
+            });
             services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("database"));
             services.AddControllers();
             services.AddScoped(typeof(GenericRepository<>), typeof(GenericRepository<>));
@@ -45,7 +53,7 @@ namespace Graff.Api
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
